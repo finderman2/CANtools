1b40b31cd0d75e6a9062b66b34ee87fa6b665e64
obrien28 <finderman2@gmail.com>
2016-02-01 18:54:01 -0500

    new version with CLI capability

--- capture.py
+++ capture.py
@@ -2,7 +2,7 @@
 
 Title: CANtact Capture
 Desc: Enables the logging of an entire bus or specific ID's with both output to file and terminal logging. It can also output SavvyCAN compatible files.
-Version: 0.3
+Version: 0.4
 Release Notes: 
 Authors: Jean-Claude Thibault, Liam O'Brien
 
@@ -10,6 +10,7 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 For a full copy of the license please vist this website: http://creativecommons.org/licenses/by-nc-sa/4.0/
 
 """
+from __future__ import print_function
 
 #Functions
 def getSerialPorts():
@@ -45,24 +46,41 @@ def getSerialPorts():
             pass
     return result
 
+def ishex(value):
+    if value[:2] == "0x":
+        return True
+    else:
+        print("Filter ID is not in hex format")
+        return False
 
-
+def process_args(args):
+    parser = argparse.ArgumentParser(
+        description="Enables the logging of an entire bus or specific ID's with both output to file and terminal logging. It can also output SavvyCAN compatible files."
+    )
+    
+    parser.add_argument('-b', '--baudrate', dest='baudrate', type=int, default=500000, help='CAN Bus Baudrate')
+    parser.add_argument('-p', '--port', dest='port', type=str, default='/dev/tty.usbmodem1421', help='CAN Bus Serial Port')
+    parser.add_argument('-w', '--write', dest='write', type=str, default='', help='Save data to a file, passing the option "date" saves the file with a filename of yyyy/mm/dd hh:mm:ss, any other option becomes the filename')
+    parser.add_argument('-l', '--log', dest='log', action='store_false', default=True, help='Enabled by default, log data to the terminal window')
+    parser.add_argument('-c', '--frame-count', dest='frame-count', type=int, default=10000, help='Number of frames to capture before terminating')
+    parser.add_argument('-f', '--filter', dest='filter', nargs='+', help='Filter the output to only the ID\'s in your filter (eg -f 0x102 0x3D2)')
+    parser.add_argument('-s', '--savvy', dest='savvy', action='store_true', default=False, help='Enable loging in a SavvyCAN compatible format')
+    parser.add_argument('-g', '--get-device', dest='getD', action='store_true', default=False, help='Output list of connected serial devices')
+    
+    options = parser.parse_args(args)
+    return vars(options)
 
 #Main Import
 import sys
 import glob
 import time, datetime
 import io
+import argparse
 
 #Windows specific modules
 if sys.platform.startswith('win'):
     import msvcrt
-    from msvcrt import getch
-    SERIAL_PORT = "COM8"
-else:
-    print("Skipping msvcrt import not running windows")
-    SERIAL_PORT = '/dev/cu.usbmodem1421'
-    
+    from msvcrt import getch    
 
 #import CANard + Serial
 from canard import can
@@ -72,12 +90,13 @@ import serial
 #User Configuration ------------------
 
 #CAN Settings
-CAN_BAUDRATE = 125000
-MAX_NUMBER_OF_FRAMES = 2000
-frame_id_filter = ['0x266'] #ID must be specified with 0x prefix
+CAN_BAUDRATE = 500000
+MAX_NUMBER_OF_FRAMES = 0
+SERIAL_PORT = '/dev/cu.usbmodem1421'
+ID_FILTER = ['0x266'] #ID must be specified with 0x prefix
 
 #Logging/Viewing Settings
-SHOW_ALL_IDs = True #True displays all ID's, False using the frame_id_filter to only display the selected ID's
+SHOW_ALL_IDs = True #True displays all ID's, False using the ID_FILTER to only display the selected ID's
 WRITE_TO_FILE = False
 SHOW_POWER_DATA = False
 LOGGING_ENABLED = True
@@ -91,20 +110,52 @@ SAVVYCAN_BUS = '0'
 SEC_BETWEEN_CAPTURE = 0
 ONLINE = True
 
+#get arguments from the CLI
+opt = process_args(sys.argv[1:])
+
 #Global Variables
 frame_counter = 0
 
-#Prints a list of available serial ports everytime the application is started
-#Can be commented out once you know which port your device is on
-#print(getSerialPorts())
+#Parse CLI arguments
+LOGGING_ENABLED = opt['log'] #logging to the terminal is enabled by default
+MAX_NUMBER_OF_FRAMES = opt['frame-count'] #max number of frames is 10000 by default
 
+if opt['baudrate'] >= 125000 and opt['baudrate'] <= 1000000:
+    CAN_BAUDRATE = opt['baudrate']
+else:
+    raise argparse.ArgumentTypeError('CAN baudrate must be between 125kb/s and 1mb/s')
+
+if opt['port'] != '':
+    SERIAL_PORT = opt['port']
+else:
+    raise argparse.ArgumentTypeError('Must set a serial port, eg COM8 or /dev/tty.*')
+
+if opt['write'] == 'date':
+    WRITE_TO_FILE = True
+elif opt['write'] != '':
+    WRITE_TO_FILE = True
+    FILE_NAME = opt['write']
+
+if opt['filter'] != []:
+    SHOW_ALL_IDs = False
+    ID_FILTER = opt['filter']
+
+if opt['savvy'] == True:
+    SAVVYCAN_FORMAT_ENABLE = True
+    print("SavvyCAN Format Enabled")
+
+if opt['getD'] == True:
+    print(getSerialPorts())
+    exit()
+
+#Begin the main program -------------------
 if ONLINE == True:
   #Setup CANtact Interface
   dev = cantact.CantactDev(SERIAL_PORT)
-  dev.set_bitrate(CAN_BAUDRATE)
+  dev.set_bitrate(CAN_BAUDRATE) 
   #Connect to the Device
   dev.start()
-  print("CANtact running on " + SERIAL_PORT + "...")
+  print("CANtact running on " + SERIAL_PORT + " at " + str(CAN_BAUDRATE) + "kb/s...")
   
 if WRITE_TO_FILE == True:
   if FILE_NAME != '':
@@ -125,7 +176,7 @@ while frame_counter <= MAX_NUMBER_OF_FRAMES:
   frame_id_hex = hex(message.id)
   
   # Filtering
-  if (frame_id_hex in frame_id_filter) or (SHOW_ALL_IDs == True and frame_id_hex!='e'):
+  if (frame_id_hex in ID_FILTER) or (SHOW_ALL_IDs == True and frame_id_hex!='e'):
     
     frame = can.Frame(frame_id)
     frame_counter = frame_counter + 1
